57eaac71d204b8c3033c7a72dafdb63d3f986a96
mustafah15 <mustafa.hussain93@gmail.com>
2021-05-20 19:39:40 +0200

    update upload route

---
+++ src/http/routes/upload.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import multer from 'multer';
+import { asyncWrapper } from '../utils/asyncWrapper';
+
+const router = express.Router();
+
+const upload = multer({ dest: 'uploads/' });
+
+export function uploadRoute() {
+  router.post(
+    '/profile',
+    upload.single('file'),
+    asyncWrapper(async (_req: Request, res: Response) => {
+      // req.file is the `avatar` file
+      // req.body will hold the text fields, if there were any
+      // const { file } = req;
+
+      res.json({ message: 'ok' });
+    }),
+  );
+
+  return router;
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export * from './errors/BadRequestError';
+export * from './errors/CustomError';
+export * from './errors/DatabaseError';
+export * from './errors/NotAuthorizedError';
+export * from './errors/NotFoundError';
+export * from './errors/RequestValidationError';
+
+export * from './middlewares/error-handlers';
+export * from './middlewares/validate-request';
