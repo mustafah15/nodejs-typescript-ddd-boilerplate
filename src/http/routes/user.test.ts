8267211a80e1e37c6fa9a24688a4159132fbbb77
mustafah15 <mustafa.hussain93@gmail.com>
2021-05-21 09:06:34 +0200

    add more test

--- src/http/routes/user.test.ts
+++ src/http/routes/user.test.ts
@@ -31,5 +31,18 @@ describe('user route test', () => {
 
       expect(user).toEqual(userData);
     });
+
+    it('should return 400 when invalid body', async () => {
+      const {
+        body: { message, details },
+      } = await request(app)
+        .post('/users')
+        .send({})
+        .expect(400);
+
+      expect(message).toEqual('Invalid Request');
+      expect(details[0]).toHaveProperty('message', 'Invalid value');
+      expect(details[0]).toHaveProperty('field', 'firstName');
+    });
   });
 });
